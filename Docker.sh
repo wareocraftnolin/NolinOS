e459540c86de2c618bbdc64ef837ac1c875e3c16
wareocraftnolin <78630991+wareocraftnolin@users.noreply.github.com>
2021-03-14 20:18:31 +0530

    de-apt-mate.sh

--- Docker.sh
+++ Docker.sh
@@ -1,10 +1,3 @@
-
-from randomdude/gcc-cross-i686-elf
-RUN apt-get update 
-RUN apt-get upgrade -y
-RUN apt-get install -y grub-common
-RUN apt-get install hostname
-RUN apt-get install kdelockscreen
-RUN apt-get install gnome
-RUN apt-get install kscreen
-RUN apt-get install plank
+apt-get install mate-desktop-environment-core mate-terminal tightvncserver -y
+apt-get install xfe -y
+apt-get clean
